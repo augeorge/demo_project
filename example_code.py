d9fb9c8c95fcbb3f42607e974a6bdf11c4e84782
Augeorge <30163079+augeorge@users.noreply.github.com>
2020-12-02 15:06:32 -0800

    update filetree

--- example_code.py
+++ example_code.py
@@ -1,1 +1,14 @@
-print('hello there')
+def func(arg1, arg2):
+    """Summary line.
+
+    Extended description of function.
+
+    Args:
+        arg1 (int): Description of arg1
+        arg2 (str): Description of arg2
+
+    Returns:
+        bool: Description of return value
+
+    """
+    return True
