08287ed62d6bc0c61757967c92d6c792732b216f
August <30163079+augeorge@users.noreply.github.com>
2020-12-02 22:07:10 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup  # pragma: no cover
 
 
 setup(
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-)
+    package_dir={'': 'src'}, 
+    packages=find_packages(where='src'),  
+)  # pragma: no cover
